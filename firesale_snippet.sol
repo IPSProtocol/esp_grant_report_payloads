57509d5193061c3e19677d6c6198ace8fa390a18
theexoticman <>
2025-09-01 16:56:49 +0200

    look improved

--- firesale_snippet.sol
+++ firesale_snippet.sol
@@ -1,28 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 contract Firesale {
-function swapWETHforFETH(
+
+    
+    function swapWETHforFETH(
         uint256 amountIn,
         uint256 amountOut,
         address[] calldata tokens,
         uint256[] calldata amounts
     ) external payable {
         uint256 ethReceived = msg.value;
-        
+
         // Swap Logic: transfer WETH from user to this contract
         WETH.transferFrom(msg.sender, address(this), amountIn);
 
-        if (fireSale) { // condition for returning or not returning tokens to user
-    
+        if (fireSale) {
+            // condition for returning or not returning tokens to user
+
             // Swap Logic: transfer FETH from this contract to user
             FETH.transfer(msg.sender, amountOut);
-    
+
             //send tokens back to user
             _giveBack(msg.sender, tokens, amounts);
-    
+
             //send eth back to user
             msg.sender.call{value: ethReceived}("");
-            
         }
     }
-    }
+}
