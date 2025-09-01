7cda0762d08fe69b637fe9523b1b02d8a75bd45e
theexoticman <>
2025-09-01 16:57:25 +0200

    final

--- firesale_snippet.sol
+++ firesale_snippet.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract Firesale {
 
-    
+
     function swapWETHforFETH(
         uint256 amountIn,
         uint256 amountOut,
@@ -15,8 +15,7 @@ contract Firesale {
         // Swap Logic: transfer WETH from user to this contract
         WETH.transferFrom(msg.sender, address(this), amountIn);
 
-        if (fireSale) {
-            // condition for returning or not returning tokens to user
+        if (fireSale) { // condition for returning or not returning tokens to user
 
             // Swap Logic: transfer FETH from this contract to user
             FETH.transfer(msg.sender, amountOut);
