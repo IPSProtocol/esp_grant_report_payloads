92af6e59d54ef87fcd81b431fcc56e64d3f11484
theexoticman <>
2025-09-01 16:56:16 +0200

    improve look

--- firesale_snippet.sol
+++ firesale_snippet.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract Firesale {
 function swapWETHforFETH(
         uint256 amountIn,
         uint256 amountOut,
@@ -21,4 +24,5 @@ function swapWETHforFETH(
             msg.sender.call{value: ethReceived}("");
             
         }
+    }
     }
