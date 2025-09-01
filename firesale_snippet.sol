4b2ada44d95a46013e0f002a5c3491eb4cfb2f8b
theexoticman <>
2025-09-01 16:54:56 +0200

    added snippets

---
+++ firesale_snippet.sol
@@ -0,0 +1,24 @@
+function swapWETHforFETH(
+        uint256 amountIn,
+        uint256 amountOut,
+        address[] calldata tokens,
+        uint256[] calldata amounts
+    ) external payable {
+        uint256 ethReceived = msg.value;
+        
+        // Swap Logic: transfer WETH from user to this contract
+        WETH.transferFrom(msg.sender, address(this), amountIn);
+
+        if (fireSale) { // condition for returning or not returning tokens to user
+    
+            // Swap Logic: transfer FETH from this contract to user
+            FETH.transfer(msg.sender, amountOut);
+    
+            //send tokens back to user
+            _giveBack(msg.sender, tokens, amounts);
+    
+            //send eth back to user
+            msg.sender.call{value: ethReceived}("");
+            
+        }
+    }
